70fbaf52775e88a2d9ede7122abf079e573aeaad
Jacob Lindgren <jlindgre@redhat.com>
2022-11-15 07:23:56 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash 
 
-oc debug deploy/sources-api-svc -t --image quay.io/jlindgren/db-debug:psql -- /app/shell.sh
+oc debug deploy/sources-api-svc -t --image quay.io/jlindgren/consoledot-debugger:master -- /app/db_shell.sh
