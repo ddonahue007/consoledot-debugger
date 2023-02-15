5b5381e38977effec4021d1ded7004c2dfd91e03
Jacob Lindgren <jlindgre@redhat.com>
2023-02-15 12:29:10 -0600

    add steps to script for shelling in and cleaning up

--- Makefile
+++ Makefile
@@ -5,3 +5,6 @@ all: push
 push:
 	podman build . -t ${IMAGE}
 	podman push ${IMAGE}
+
+debug:
+	bash run.sh ${DBSECRET}
--- run.sh
+++ run.sh
@@ -7,3 +7,7 @@ if [[ -z $1 ]]; then
 fi
 
 cat ./templates/db-debug-pod.yml | sed "s/DBSECRET/$1/g" | k create -f - 
+
+kubectl wait --for=condition=Ready pod/db-debug
+kubectl exec -it db-debug -- bash
+kubectl delete -f ./templates/db-debug-pod.yml
