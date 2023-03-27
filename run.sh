ce7cf8e214397e3aff2c2e46136baf6fde9c8060
Jacob Lindgren <jlindgre@redhat.com>
2023-03-27 10:11:55 -0500

    don't wait for pod to be deleted

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ cat ./templates/db-debug-pod.yml | sed "s/DBSECRET/$1/g" | k create -f -
 
 kubectl wait --for=condition=Ready pod/db-debug
 kubectl exec -it db-debug -- bash
-kubectl delete -f ./templates/db-debug-pod.yml
+kubectl delete -f ./templates/db-debug-pod.yml --wait=false
